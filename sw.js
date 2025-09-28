fd825995554ec872b76777e0d96660256405e4d9
SAMUEL ALVES DE MELO JUNIOR <samuelamjr@gmail.com>
2025-09-28 08:48:48 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'roteirizador-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instala o service worker e faz cache dos arquivos principais
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Ativa o service worker e limpa caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => {
+        if (key !== CACHE) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+});
+
+// Intercepta requisições e serve do cache quando possível
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
